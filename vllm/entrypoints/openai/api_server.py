464b95a0e3822282950aeffc80e456dbe830eba7
付 <fulei@ratutech.com>
2025-03-24 15:11:10 +0800

    update

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -474,7 +474,7 @@ async def create_completion(request: CompletionRequest, raw_request: Request):
             message="The model does not support Completions API")
 
     generator = await handler.create_completion(request, raw_request)
-    print(f"lei test Debug choices: {generator.choices} \n")
+    logger.debug("lei test Debug choices: %s",generator.choices)
     if isinstance(generator, ErrorResponse):
         return JSONResponse(content=generator.model_dump(),
                             status_code=generator.code)
